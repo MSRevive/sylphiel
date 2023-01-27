ba8ac7e6d8925969c0725c5f33d340df8ba9fafa
Saint Wish <SaintWish@users.noreply.github.com>
2023-01-26 21:15:33 -0700

    event stuff that doesn't work

--- internal/events/events.go
+++ internal/events/events.go
@@ -1,6 +1,8 @@
 package events
 
 import (
+	"fmt"
+
 	"github.com/msrevive/sylphiel/cmd/dbot"
 
 	"github.com/disgoorg/disgo/events"
@@ -13,5 +15,18 @@ func OnReady(b *dbot.Bot) bot.EventListener {
 		e.Client().Rest().CreateMessage(b.Config.Discord.DevChannel, discord.NewMessageCreateBuilder().SetContent("Bot loaded.").Build())
 	})*/
 	return bot.NewListenerFunc(func(e *events.Ready) {
+		b.Logger.Debug("OnReady event called")
+	})
+}
+
+func GuildMemberJoin(b *dbot.Bot) bot.EventListener {
+	return bot.NewListenerFunc(func(e *events.GuildMemberJoin) {
+		b.Logger.Debug("GuildMemberJoin event called")
+		if e.GuildID == b.Config.Discord.GuildID {
+			if b.Config.Discord.DefaultRole != 0 && !e.Member.User.Bot {
+				e.Client().Rest().AddMemberRole(e.GuildID, e.Member.User.ID, b.Config.Discord.DefaultRole)
+				fmt.Println("AddMemberGuild")
+			}
+		}
 	})
 }
