2463aa4033d09affcd7003b6bf3b5b9bc56f19a7
Saint Wish <SaintWish@users.noreply.github.com>
2023-05-13 00:58:29 -0600

    work on roles command

--- internal/commands/setup.go
+++ internal/commands/setup.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"github.com/msrevive/sylphiel/cmd/dbot"
 	"github.com/msrevive/sylphiel/internal/response"
 
 	"github.com/disgoorg/disgo/discord"
@@ -22,8 +23,33 @@ var setup = discord.SlashCommandCreate {
 	},
 }
 
-func HandleRolesSetup(e *handler.CommandEvent) error {
-	return response.NotImplemented(e)
+func HandleRolesSetup(b *dbot.Bot) handler.CommandHandler {
+	return func(e *handler.CommandEvent) error {
+		embed := discord.NewEmbedBuilder().
+			SetColor(0x7851a9).
+			AddField(":studio_microphone: - Voice Acting", "", false).
+			AddField(":hammer: - Mapping", "", false).
+			AddField("<:blender:1106808241633120317> - Modeling/Animation", "", false).
+		Build()
+
+		msgBuilder := discord.NewMessageCreateBuilder().
+			SetContent("React to this message if you're interested in helping us.").
+			SetEmbeds(embed).
+		Build()
+
+		msg, err := e.Client().Rest().CreateMessage(e.ChannelID(), msgBuilder)
+		if err != nil {
+			b.Logger.Error(err)
+			return response.Error(e, err)
+		}
+
+		if err := e.Client().Rest().AddReaction(e.ChannelID(), msg.ID, ":hammer:"); err != nil {
+			b.Logger.Error(err)
+			return response.Error(e, err)
+		}
+
+		return nil
+	}
 }
 
 func HandleServerListSetup(e *handler.CommandEvent) error {
