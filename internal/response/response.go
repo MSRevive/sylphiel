a18a14a6572ae65948976d9c4b7f0557e49421d7
Saint Wish <SaintWish@users.noreply.github.com>
2023-05-13 16:21:51 -0600

    command success response

--- internal/response/response.go
+++ internal/response/response.go
@@ -40,7 +40,7 @@ func Error(e *handler.CommandEvent, err error) error {
 
 func NoPermission(e *handler.CommandEvent) error {
 	embed := discord.NewEmbedBuilder().
-		SetTitle("No Permission").
+		SetTitle("Incorrect Permission").
 		SetColor(0xcc0000).
 		SetDescription("You don't have permission to this command!").
 		SetTimestamp(time.Now()).
@@ -49,6 +49,22 @@ func NoPermission(e *handler.CommandEvent) error {
 
 	return e.Respond(discord.InteractionResponseTypeCreateMessage, discord.NewMessageCreateBuilder().
 		SetEphemeral(true).
+		SetEmbeds(embed).
+		Build(),
+	)
+}
+
+func Success(e *handler.CommandEvent, msg string, emp bool) error {
+	embed := discord.NewEmbedBuilder().
+		SetTitle("Command Successful").
+		SetColor(0x009999).
+		SetDescription(msg).
+		SetTimestamp(time.Now()).
+		SetFooter("Sylphiel", "https://winterfang.com/assets/gfx/bot-avatar.png").
+	Build()
+
+	return e.Respond(discord.InteractionResponseTypeCreateMessage, discord.NewMessageCreateBuilder().
+		SetEphemeral(emp).
 		SetEmbeds(embed).
 		Build(),
 	)
