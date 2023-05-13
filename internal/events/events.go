ff9da2af3a90ff54c1d7d7c6bbf8cc888e6e3034
Saint Wish <SaintWish@users.noreply.github.com>
2023-05-13 17:00:14 -0600

    add presence

--- internal/events/events.go
+++ internal/events/events.go
@@ -2,13 +2,15 @@ package events
 
 import (
 	//"fmt"
+	"context"
 
 	"github.com/msrevive/sylphiel/cmd/dbot"
 	//"github.com/msrevive/sylphiel/internal/response"
 
 	"github.com/disgoorg/disgo/events"
 	"github.com/disgoorg/disgo/bot"
-	//"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/gateway"
 )
 
 func OnReady(b *dbot.Bot) bot.EventListener {
@@ -16,7 +18,12 @@ func OnReady(b *dbot.Bot) bot.EventListener {
 	// 	e.Client().Rest().CreateMessage(b.Config.Disc.DevChannel, discord.NewMessageCreateBuilder().SetContent("Bot loaded.").Build())
 	// })
 	return bot.NewListenerFunc(func(e *events.Ready) {
-		b.Logger.Debug("OnReady event called")
+		if err := b.Client.SetPresence(context.TODO(),
+			gateway.WithListeningActivity("gobby slobby"),
+			gateway.WithOnlineStatus(discord.OnlineStatusOnline),
+		); err != nil {
+			b.Logger.Errorf("Failed to set presence: %s", err)
+		}
 	})
 }
 
